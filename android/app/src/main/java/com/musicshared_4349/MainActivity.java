6fb24943cec5f330201e7757dcb67cbe1ad3237e
Crowdbotics <team@crowdbotics.com>
2019-06-06 23:42:14 +0000

    initial commit

---
+++ android/app/src/main/java/com/musicshared_4349/MainActivity.java
@@ -0,0 +1,15 @@
+package com.musicshared_4349;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "musicshared_4349";
+    }
+}
